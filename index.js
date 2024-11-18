eb2b1e5e1e329ac72d6b38abba0aa63d397e0045
JuanM27 <juanmanuel2727@gmail.com>
2024-11-18 16:14:17 +0100

    Cambio puerto a 8080

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app/app.js');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
